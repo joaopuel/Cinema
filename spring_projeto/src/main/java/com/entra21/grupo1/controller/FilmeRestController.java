166295b82d8efa073f28d9f0569aa53b8133d7d3
João Ricardo Tasca Puel <jrtpuel@furb.br>
2022-09-13 21:41:47 -0300

    Sessoes_p/_filme_e_filtro_filme_p/_sessao

--- spring_projeto/src/main/java/com/entra21/grupo1/controller/FilmeRestController.java
+++ spring_projeto/src/main/java/com/entra21/grupo1/controller/FilmeRestController.java
@@ -3,12 +3,14 @@ package com.entra21.grupo1.controller;
 import com.entra21.grupo1.model.dto.FilmeDTO;
 import com.entra21.grupo1.view.service.FilmeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @RestController
@@ -19,7 +21,7 @@ public class FilmeRestController {
     private FilmeService filmeService;
 
     @GetMapping
-    public List<FilmeDTO> getAllFilmes(@RequestParam(name = "dataSessao", required = false) LocalDateTime dataSessao) {
+    public List<FilmeDTO> getAllFilmes(@RequestParam(name = "dataSessao", required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime dataSessao) {
         return filmeService.getAll(dataSessao);
     }
 }
