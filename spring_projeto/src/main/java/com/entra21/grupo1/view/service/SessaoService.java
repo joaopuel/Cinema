f61aed0dd2a84e498c048f12231c1115ad36a858
João Ricardo Tasca Puel <jrtpuel@furb.br>
2022-09-15 21:47:50 -0300

    FilmesP/Sessao

--- spring_projeto/src/main/java/com/entra21/grupo1/view/service/SessaoService.java
+++ spring_projeto/src/main/java/com/entra21/grupo1/view/service/SessaoService.java
@@ -1,10 +1,12 @@
 package com.entra21.grupo1.view.service;
 
 import com.entra21.grupo1.model.dto.SessaoDTO;
+import com.entra21.grupo1.model.entity.SessaoEntity;
 import com.entra21.grupo1.view.repository.SessaoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,8 +16,14 @@ public class SessaoService {
     @Autowired
     private SessaoRepository sessaoRepository;
 
-    public List<SessaoDTO> getAll(){
-        return sessaoRepository.findAll().stream().map( sessao -> {
+    public List<SessaoDTO> getAll(LocalDateTime dataSessao){
+        List<SessaoEntity> list;
+        if(dataSessao == null){
+            list = sessaoRepository.findAll();
+        }else{
+            list = sessaoRepository.findAllByDataSessaoBetween(dataSessao.toLocalDate().atStartOfDay(), dataSessao.toLocalDate().plusDays(1).atStartOfDay());
+        }
+        return list.stream().map( sessao -> {
             SessaoDTO dto = new SessaoDTO();
             dto.setId(sessao.getId());
             dto.setDataSessao(sessao.getDataSessao());
