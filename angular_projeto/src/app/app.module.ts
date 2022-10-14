7f2ce989af666dcbef3703365e95f0afbcf32740
joao puel <you@example.com>
2022-10-14 17:15:38 -0300

    updt

--- angular_projeto/src/app/app.module.ts
+++ angular_projeto/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule, AppRoutingModule
