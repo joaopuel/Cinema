e23f40fe3b2e49ca46168ec61ee63a6b21d16bf0
joao puel <you@example.com>
2022-10-19 17:16:00 -0300

    upd

--- angular_projeto/src/app/sessoes-page/sessoes-screen/sessoes-screen.component.ts
+++ angular_projeto/src/app/sessoes-page/sessoes-screen/sessoes-screen.component.ts
@@ -7,9 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SessoesScreenComponent implements OnInit {
 
+  diaSemana: string[] = ["Dom","Seg","Ter","Qua","Qui","Sex","Sáb"];
+
+  dataHoje: string = new Date().getDate() + '/' + new Date().getMonth();
+
   constructor() { }
 
   ngOnInit(): void {
   }
+  
+  getDays = (primeiro: boolean) => {
+    var listaDias: Date[] = [];
 
+    if(primeiro){
+      for(let i=1; i<4; i++){
+        let nextDay = new Date();
+        nextDay.setDate(nextDay.getDate()+i);
+        listaDias.push(nextDay);
+      }
+    } else {
+      for(let i=4; i<9; i++){
+        let nextDay = new Date();
+        nextDay.setDate(nextDay.getDate()+i);
+        listaDias.push(nextDay);
+      }
+    }
+
+    return listaDias;
+  }
 }
