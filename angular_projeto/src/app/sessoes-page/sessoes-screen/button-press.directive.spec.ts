864db1da26e1579cb44053ac3a5cc4e74c4fe9a0
Andre <andre@pop-os.localdomain>
2022-10-22 11:23:24 -0300

    sono

---
+++ angular_projeto/src/app/sessoes-page/sessoes-screen/button-press.directive.spec.ts
@@ -0,0 +1,8 @@
+import { ButtonPressDirective } from './button-press.directive';
+
+describe('ButtonPressDirective', () => {
+  it('should create an instance', () => {
+    const directive = new ButtonPressDirective();
+    expect(directive).toBeTruthy();
+  });
+});
